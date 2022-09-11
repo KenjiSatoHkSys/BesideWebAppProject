594fb7ab6ce67f0d92a3b3dd499ec647fe7bc788
Kenji Sato <kenji1.satou@daikin.co.jp>
2022-09-11 12:14:01 +0900

    comment modified

--- BesideWebApp/urls.py
+++ BesideWebApp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'BesideWebApp'
 
+# Herokuでは24h周期でアプリ(dyno)の再起動が行われる。
+# ログイン処理を持たせている場合、再起動後のリクエストに対しては Not found が画面に表示され停止してしまう。
+# よって、ログイン処理はなしにする必要あり。
+
 urlpatterns = [
 #   path('', views.viewslogin, name='viewslogin'),
 #   path('logout', views.viewslogout, name='viewslogout'),
